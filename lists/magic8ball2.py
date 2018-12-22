343db3a2c1b4de7a829027ee33d841492fd1622f
munna-bhai <amankashyap520@gmail.com>
2018-12-22 16:16:59 +0530

    magic ball using list

---
+++ lists/magic8ball2.py
@@ -0,0 +1,7 @@
+#magic ball
+
+import random   #import random package
+
+messages = ['It is certain', 'It is decidedly so', 'i am batman', 'modi modi modi', 'ha mai pappu hu', 'chai pi lo']
+
+print (messages[random.randint(0,len(messages) -1)])    #print value in list 'messages' takes one random index and prints its value
