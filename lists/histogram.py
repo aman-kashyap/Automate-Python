c9b5f9822ff874af9fba14ee1e5ebbb178af88bb
munna-bhai <amankashyap520@gmail.com>
2018-12-24 00:20:39 +0530

    lab assignment 2

--- lists/histogram.py
+++ lists/histogram.py
@@ -1,9 +1,18 @@
 #histogram
 
 def histogram(number):
-    for number[i] in range(int(number)):
-        print('*'*int(number[i]))
+    #for number[i] in range(int(number)):
+    #    print('*'*int(number[i]))
+    print(number)
 
 
-i=[2,3,4,5]
-histogram(i)
+p=[2,3,4,5]
+histogram(p)
+#i=p.index()
+print(p[:])
+print (len(p))
+for i in p[:]:
+    print('*'*i)
+
+#for p[:] in range(len(p)):
+#    print ('*'*p[:])
