bb84983ede7aecf2339d10158ed0428cea0945f0
munna-bhai <amankashyap520@gmail.com>
2018-12-24 01:01:47 +0530

    lab assignment 2 histogram complete

--- lists/histogram.py
+++ lists/histogram.py
@@ -1,13 +1,28 @@
 #histogram
 
-def histogram(number):
-    for i in number[:]:
-        print('*'*i)
-    #print(number)
+def histogram(number):  #define a function 
+    for i in number[:]: #here i picks all values in list.
+        print('*'*i)    
 
+p=[]    #initialize a list
 
-p=[2,3,4,5]
-histogram(p)
+print ("enter some integer for making histogram and to stop press 0")
+
+while True:
+    
+    integer=int(input()) #takes integer input for the list 
+    if integer ==0:
+        break               #breaks loop to exit condition and completes list p[]
+    p=p+[integer]           #list concatenation
+
+print("your list contains : "+str(p))
+histogram(p)    #calling function
+
+#print("you have entered following integers")
+#for integer in p:
+#    print(integer)
+#p=[2,3,4,5]
+#histogram(p)
 
 #print(p[:])
 #print (len(p))
