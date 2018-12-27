fd7ef6ba5acb2e7de88a9d9989dc8430cd063918
munna-bhai <amankashyap520@gmail.com>
2018-12-27 13:03:47 +0530

    birthday dictionary database

--- dictionary/friendBirthday.py
+++ dictionary/friendBirthday.py
@@ -8,4 +8,13 @@ while True:
     if name=="":
         break
     if name in birthday:
-        print(name+'has birthday on'+birthday[name])
+        print(name+' has birthday on '+birthday[name])
+
+    else:
+        print(name +" is not in birthday dictionary")
+        print("what is the birthday of the name given by you")
+        bday=input()
+        birthday[name]=bday     #here name is key and bday is its value in the birthday dictionary and it saves the information until the program exits
+        print("have updated birthday database temporarily")
+
+    
