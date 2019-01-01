260c9ffa6ad186f834506ca84e70883764e771db
munna-bhai <amankashyap520@gmail.com>
2019-01-01 19:21:50 +0530

    added comment

--- dictionary/chracterCount.py
+++ dictionary/chracterCount.py
@@ -2,10 +2,11 @@
 
 message = 'Respect your elders follow their teachings and it will help you in your tough times'
 
-count={}
+count={}    #count dictionary
 
 for character in message:
-    count.setdefault(character,0)
+    count.setdefault(character,0) #the setdefault() method ensures that the key is in count dictionary with a default value 0 
     count[character]=count[character]+1
+
 
 print(count)
