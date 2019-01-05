670484399e300ed4edd9584df4b7b24b65f1de53
munna-bhai <amankashyap520@gmail.com>
2019-01-05 16:32:07 +0530

    basic string manipulation

---
+++ stringManipulation/string1.py
@@ -0,0 +1,32 @@
+#string manipulation basic example
+
+
+print ("hello world! this is \"MOTHER\" the nature's beauty")   #using double qoute escape 
+
+print ('hello world! this is the nature\'s beauty')     #using escape character
+
+print ('hello world! thi\ts is the nature\'s beauty')   #using tab character
+
+print ('hello world!\n this is the nature\'s beauty')   #using newline break
+
+print ('hello world! \\this is the nature\'s beauty')   #using backslash escape
+
+print (r'hello world! this is the nature\'s beauty')     #using raw string
+
+print(''' this is 
+multiline string which donot
+use
+newline break''')   #using triple single quotes
+
+"""this is 
+multiline string which donot
+use
+newline break"""        #using triple double quotes for multiline comments
+
+def spam():
+    """this is 
+    multiline string which donot
+    use
+    newline spam() break"""
+    print('hello!')
+
