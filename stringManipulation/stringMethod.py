0a4c3558f237b31a8c8b46517e5f906750cc3515
munna-bhai <amankashyap520@gmail.com>
2019-01-05 17:36:41 +0530

    using string method upper(), lower()

---
+++ stringManipulation/stringMethod.py
@@ -0,0 +1,14 @@
+# string methods
+
+spam = 'heLlo world!'
+
+print(spam.upper())     #upper() converts all letter to uppercase
+
+print(spam.lower())     #lower() converts all letter to lowercase
+
+print ("Hey! How are you?")
+answer=input()
+if answer.lower()=='great':
+    print("I feel great too")
+else:
+    print("hope your rest of the day is good.")
