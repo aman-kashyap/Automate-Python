54f62d09d89991e7d6a2cc1882bbac366b22239f
munna-bhai <amankashyap520@gmail.com>
2019-01-12 11:36:55 +0530

    strip whitspace left and right of string

---
+++ stringManipulation/strip.py
@@ -0,0 +1,8 @@
+# striping white spaces
+
+spam = "            hey you champ                 "
+print(spam.strip())
+print(spam.lstrip())
+print(spam.rstrip())
+pot='amanisback'
+print(pot.strip('a'))
