e4d8cca7493778582260b267afa71444f4889f2c
munna-bhai <amankashyap520@gmail.com>
2019-02-11 11:03:18 +0530

    no change

--- stringManipulation/stringMethod2.py
+++ stringManipulation/stringMethod2.py
@@ -10,7 +10,7 @@ cities=['pune', 'patna', 'gaya', 'benaras', 'prayagraj'] #a list
 print(cities)
 
 print(', '.join(cities))    #join() method joins list of string together in a single string value 
-print('yo '.join(cities))
+print('YO'.join(cities))
 #print(type(a))
 
 print(spam.split())         #split() method splits a string into a list
