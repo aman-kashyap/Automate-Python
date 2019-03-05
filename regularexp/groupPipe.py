9d233760a277a81bc96eda39b5089e2ae4d64ad4
munna-bhai <amankashyap520@gmail.com>
2019-03-05 18:06:02 +0530

    match pattern with prefix

--- regularexp/groupPipe.py
+++ regularexp/groupPipe.py
@@ -6,7 +6,17 @@ import re
 #using pipe to separate pattern
 heroRegex = re.compile(r'Capt. Vikram Batra | Virat Kohli')
 
-hero='Capt. Vikram Batra and Virat Kohli'
+hero='Capt. Vikram Batra is real Hero not Virat Kohli'
 myhero = heroRegex.search(hero)
 
-print (myhero.group())
+print (myhero.group()+" is my HERO ")
+
+# match pattern beginning with same letter in a word.
+batRegex = re.compile(r'Bat(man|copter|mobile)')
+
+joker='ha ha ha your Batcopter is toy to me Batman'
+
+myBat= batRegex.search(joker)
+
+print(myBat.group())    #returns first matched character along with prefix
+print(myBat.group(1))   #return only matched character
